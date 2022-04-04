03c1038eef4aa7a195a99291aec7061c847ebcc8
asi-isa <ali.inceoglu@protonmail.com>
2022-04-04 17:54:51 +0200

    badge

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import styles from "../styles/Dashboard.module.css";
 import SideMenu from "../comps/menu/SideMenu";
+import { IoAppsOutline, IoApps } from "react-icons/io5";
 
 export default function Dashboard() {
   const [showSideMenu, setShowSideMenu] = useState(false);
@@ -11,12 +12,18 @@ export default function Dashboard() {
       <SideMenu showSideMenu={showSideMenu} />
 
       <div className={styles.main}>
-        <h1
-          className={styles.title}
+        <div
+          className={styles.badge}
           onClick={() => setShowSideMenu(!showSideMenu)}
         >
-          Hello, isa!
-        </h1>
+          {!showSideMenu ? (
+            <IoApps size={28} color="#fbf8f1" />
+          ) : (
+            <IoAppsOutline size={28} color="#fbf8f1" />
+          )}
+        </div>
+
+        <h1 className={styles.title}>Hello, isa!</h1>
       </div>
     </div>
   );
